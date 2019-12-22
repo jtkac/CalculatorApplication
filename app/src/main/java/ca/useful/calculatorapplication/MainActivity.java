49371fbbe2a8e27ac67ddff6a623bf2ae14a379d
jtkachuk <derpmcderpson2@gmail.com>
2019-12-21 17:53:17 -0700

    Initial Commit 2

---
+++ app/src/main/java/ca/useful/calculatorapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.useful.calculatorapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
